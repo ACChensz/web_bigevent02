cb19348158aa5aa463148a2d76758dd9a16808b3
acchen <acchen1900@163.com>
2021-09-02 11:45:52 +0800

    finish login & reg

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,5 +1,5 @@
 $(function() {
-    // 点击去注册，跳转注册界面
+    // 1点击去注册，跳转注册界面
     $('.link-reg').on('click', function() {
         $('.reg-box').show().siblings('.login-box').hide();
     });
@@ -8,9 +8,10 @@ $(function() {
         $('.reg-box').hide().siblings('.login-box').show();
     });
 
-    // 从 layui 中获取 form 对象
+    // 从 layui 中获取 form  layer对象
     var form = layui.form;
-    //自定义表单验证
+    var layer = layui.layer;
+    //2自定义表单验证
     form.verify({
         username: function(value, item) { //value：表单的值、item：表单的DOM对象
             if (!new RegExp("^[a-zA-Z0-9_\u4e00-\u9fa5\\s·]+$").test(value)) {
@@ -48,6 +49,64 @@ $(function() {
             }
         }
     });
+
+    /*  TODO:FIXME://3监听注册表单的提交事件
+     $('#form_reg').on('submit', function(e) {
+         //阻止默认提交行为
+         e.preventDefault();
+
+         //发起ajax的post请求
+         $.post('/api/reguser', { username: $('#form_reg [name=username]').val(), password: $('#form_reg [name=password]').val() }, function(res) {
+             if (res !== 0) {
+                 return layer.msg(res.message)
+             }
+             layer.msg('注册成功，请登录！')
+                 // 模拟人的点击行为,返回登录界面
+             $('#link_login').click()
+         })
+     }) */
+
+    // 监听注册表单的提交事件
+    $('#form_reg').on('submit', function(e) {
+        // 1. 阻止默认的提交行为
+        e.preventDefault()
+            // 2. 发起Ajax的POST请求
+        var data = {
+            username: $('#form_reg [name=username]').val(),
+            password: $('#form_reg [name=password]').val()
+        }
+        $.post('/api/reguser', data, function(res) {
+            if (res.status !== 0) {
+                return layer.msg(res.message)
+            }
+            layer.msg('注册成功，请登录！')
+                // 模拟人的点击行为,返回登录界面
+            $('#link_login').click()
+        })
+    })
+
+    // 监听登录表单的提交事件
+    $('#form_login').submit(function(e) {
+        // 阻止默认提交行为
+        e.preventDefault()
+        $.ajax({
+            url: '/api/login',
+            method: 'POST',
+            // 快速获取表单中的数据
+            data: $(this).serialize(),
+            success: function(res) {
+                if (res.status !== 0) {
+                    return layer.msg('登录失败！')
+                }
+                layer.msg('登录成功！')
+                    // 将登录成功得到的 token 字符串，保存(保存数据函数setItem())到 localStorage 中
+                localStorage.setItem('token', res.token)
+                    // 跳转到后台主页
+                location.href = '/index.html'
+            }
+        })
+    })
+
 
 
 })
